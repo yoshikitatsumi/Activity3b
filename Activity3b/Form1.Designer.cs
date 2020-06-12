3aa85cc54bc22380f50e14bade477e2e9b68ac0e
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-06-12 12:18:27 +1200

    changes

---
+++ Activity3b/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Activity3b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button = new System.Windows.Forms.Button();
+            this.Seconds = new System.Windows.Forms.TextBox();
+            this.First = new System.Windows.Forms.TextBox();
+            this.Fifth = new System.Windows.Forms.TextBox();
+            this.Tenth = new System.Windows.Forms.TextBox();
+            this.Twentieth = new System.Windows.Forms.TextBox();
+            this.Fiftieth = new System.Windows.Forms.TextBox();
+            this.Hundredth = new System.Windows.Forms.TextBox();
+            this.Money = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(526, 129);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(75, 23);
+            this.Button.TabIndex = 0;
+            this.Button.Text = "Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Seconds
+            // 
+            this.Seconds.Location = new System.Drawing.Point(686, 344);
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(100, 22);
+            this.Seconds.TabIndex = 1;
+            // 
+            // First
+            // 
+            this.First.Location = new System.Drawing.Point(815, 344);
+            this.First.Name = "First";
+            this.First.Size = new System.Drawing.Size(100, 22);
+            this.First.TabIndex = 2;
+            // 
+            // Fifth
+            // 
+            this.Fifth.Location = new System.Drawing.Point(554, 344);
+            this.Fifth.Name = "Fifth";
+            this.Fifth.Size = new System.Drawing.Size(100, 22);
+            this.Fifth.TabIndex = 3;
+            // 
+            // Tenth
+            // 
+            this.Tenth.Location = new System.Drawing.Point(424, 344);
+            this.Tenth.Name = "Tenth";
+            this.Tenth.Size = new System.Drawing.Size(100, 22);
+            this.Tenth.TabIndex = 4;
+            // 
+            // Twentieth
+            // 
+            this.Twentieth.Location = new System.Drawing.Point(304, 344);
+            this.Twentieth.Name = "Twentieth";
+            this.Twentieth.Size = new System.Drawing.Size(100, 22);
+            this.Twentieth.TabIndex = 5;
+            // 
+            // Fiftieth
+            // 
+            this.Fiftieth.Location = new System.Drawing.Point(172, 344);
+            this.Fiftieth.Name = "Fiftieth";
+            this.Fiftieth.Size = new System.Drawing.Size(100, 22);
+            this.Fiftieth.TabIndex = 6;
+            // 
+            // Hundredth
+            // 
+            this.Hundredth.Location = new System.Drawing.Point(54, 344);
+            this.Hundredth.Name = "Hundredth";
+            this.Hundredth.Size = new System.Drawing.Size(100, 22);
+            this.Hundredth.TabIndex = 7;
+            // 
+            // Money
+            // 
+            this.Money.Location = new System.Drawing.Point(501, 37);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(100, 22);
+            this.Money.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "money";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(812, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(683, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(301, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "20";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(169, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "50";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Money);
+            this.Controls.Add(this.Hundredth);
+            this.Controls.Add(this.Fiftieth);
+            this.Controls.Add(this.Twentieth);
+            this.Controls.Add(this.Tenth);
+            this.Controls.Add(this.Fifth);
+            this.Controls.Add(this.First);
+            this.Controls.Add(this.Seconds);
+            this.Controls.Add(this.Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox Seconds;
+        private System.Windows.Forms.TextBox First;
+        private System.Windows.Forms.TextBox Fifth;
+        private System.Windows.Forms.TextBox Tenth;
+        private System.Windows.Forms.TextBox Twentieth;
+        private System.Windows.Forms.TextBox Fiftieth;
+        private System.Windows.Forms.TextBox Hundredth;
+        private System.Windows.Forms.TextBox Money;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Activity3b/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity3b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var total = Convert.ToInt32(Money.Text);
+
+            Hundredth.Text = total / 100 + "";
+            var remain = Convert.ToInt32(Hundredth.Text) * 100;
+            total = total - remain;
+            Fiftieth.Text = total / 50 + "";
+            remain = Convert.ToInt32(Fiftieth.Text) * 50;
+            total = total - remain;
+            Twentieth.Text = total / 20 + "";
+            remain = Convert.ToInt32(Twentieth.Text) * 20;
+            total = total - remain;
+            Tenth.Text = total / 10 + "";
+            remain = Convert.ToInt32(Tenth.Text) * 10;
+            total = total - remain;
+            Fifth.Text = total / 5 + "";
+            remain = Convert.ToInt32(Fifth.Text) * 5;
+            total = total - remain;
+            Seconds.Text = total / 2 + "";
+            remain = Convert.ToInt32(Seconds.Text) * 2;
+            total = total - remain;
+            First.Text = total + "";
+
+
+
+
+
+        }
+    }
+}
